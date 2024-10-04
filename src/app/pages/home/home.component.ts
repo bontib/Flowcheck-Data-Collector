423633a9cad0ed088bca710c75734d6088b67d01
bontib <bonmoolenburgh@gmail.com>
2024-10-04 10:45:08 +0200

    state change and getting event

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal, WritableSignal } from '@angular/core';
 import { FileuploadComponent } from "../../features/fileupload/fileupload.component";
 import { ToolbarComponent } from '../../shared/components/toolbar/toolbar.component';
+import { Events } from './enum/events.enum';
 
 @Component({
   selector: 'check-home',
@@ -13,5 +14,12 @@ import { ToolbarComponent } from '../../shared/components/toolbar/toolbar.compon
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  states = Events;
+  srcFoto: WritableSignal<Blob | undefined> = signal(undefined);
+  pageState: WritableSignal<Events> = signal(Events.default);
 
+  setSrcFoto(event: Blob) {
+    this.srcFoto.update(s => event);
+    this.pageState.update(s => Events.foto_edit);
+  }
 }
